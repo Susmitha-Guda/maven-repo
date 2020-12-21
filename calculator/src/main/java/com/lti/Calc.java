ab84dc8e42d698e34fb920b3a137985d2e2a0eac
Susmitha-Guda <gudasusmitha99@gmail.com>
2020-12-21 15:16:50 +0530

    updated div

--- calculator/src/main/java/com/lti/Calc.java
+++ calculator/src/main/java/com/lti/Calc.java
@@ -12,12 +12,16 @@ public class Calc {
 	public static int mul(int num1,int num2) {
 		return num1*num2;
 	}
+	public static int div(int num1,int num2) {
+		return num1/num2;
+	}
 	
 	public static void main(String[] args)
 	{
 		System.out.println(add(10,20));
 		System.out.println(diff(50,20));
 		System.out.println(mul(50,20));
+		System.out.println(div(50,25));
 	}
 
 }
