ab63e392ac55c0824d194b4b273201685a53ca52
Susmitha-Guda <gudasusmitha99@gmail.com>
2020-12-21 15:09:30 +0530

    updated mul

--- calculator/src/main/java/com/lti/Calc.java
+++ calculator/src/main/java/com/lti/Calc.java
@@ -9,11 +9,15 @@ public class Calc {
 	public static int diff(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int mul(int num1,int num2) {
+		return num1*num2;
+	}
 	
 	public static void main(String[] args)
 	{
 		System.out.println(add(10,20));
 		System.out.println(diff(50,20));
+		System.out.println(mul(50,20));
 	}
 
 }
